034f50da4b7cb7a47e0b135e5600a61222d85a44
Charleu <sdlimeira0@gmail.com>
2024-10-15 19:20:23 -0300

    Primeiro commit

---
+++ ExemploCrud/Servico/ClienteServico.cs
@@ -0,0 +1,42 @@
+﻿using ExemploCrud.Core.Entidades;
+using ExemploCrud.Infra.Repositorio;
+
+namespace ExemploCrud.Servico
+{
+    internal class ClienteServico
+    {
+        private readonly ClienteRepositorio _clienteRepositorio;
+
+        public ClienteServico(ClienteRepositorio clienteRepositorio)
+        {
+            _clienteRepositorio = clienteRepositorio;
+        }
+
+        public void AdicionarCliente(Cliente cliente)
+        {
+            _clienteRepositorio.AdicionarCliente(cliente);
+        }
+
+        public void EditarCliente(int clienteId, string novoNome, string novoEmail, DateTime novaDataNascimento)
+        {
+            var cliente = _clienteRepositorio.GetClientes().FirstOrDefault(x => x.Id == clienteId);
+            if (cliente != null)
+            {
+                cliente.Nome = novoNome;
+                cliente.Email = novoEmail;
+                cliente.DataNascimento = novaDataNascimento;
+                _clienteRepositorio.AtualizarCliente(cliente);
+            }
+        }
+
+        public void DeleteClienteFisico(int clienteId)
+        {
+            _clienteRepositorio.DeleteClienteFisico(clienteId);
+        }
+
+        public List<Cliente> GetClientes()
+        {
+            return _clienteRepositorio.GetClientes();
+        }
+    }
+}
